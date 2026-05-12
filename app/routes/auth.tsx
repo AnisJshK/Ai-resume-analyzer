f4444dbeffdf862e1ea081d3a5ff26977b33dd86
AnisJshk <anisshaikh2131@gmail.com>
2026-05-12 10:45:08 +0530

    initialize auth

---
+++ app/routes/auth.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const meta = () => ([
+  {title:'NeuraHire | Auth'},
+  {name:'description',content:'Log into your account'}
+])
+
+const Auth = () => {
+  return (
+    <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center h-screen w-screen">
+
+    </main>
+  )
+}
+
+export default Auth
