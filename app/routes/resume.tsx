336429bf08e9bd26256066b6647d8ab8cd24215e
AnisJshk <anisshaikh2131@gmail.com>
2026-05-13 13:04:35 +0530

    resume-page-complete

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
 import { usePuterStore } from "~/lib/puter";
+import Summary from "../../components/Summary";
+import ATS from "../../components/ATS";
+import Details from "../../components/Details";
 
 export const meta = ()=>([
   {title:'NeuraHire | Review'},
@@ -12,7 +15,7 @@ const Resume = () => {
   const { auth, isLoading, fs, kv } = usePuterStore();
   const [imageUrl, setImageUrl] = useState("");
   const [resumeUrl, setResumeUrl] = useState("");
-  const [feedback, setFeedback] = useState("");
+  const [feedback, setFeedback] = useState<Feedback|null>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -107,7 +110,9 @@ const Resume = () => {
             <div
               className={"flex flex-col gap-8 animate-in fade-in duration-1000"}
             >
-              Summary ATS Details
+             <Summary feedback={feedback} />
+              <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []}/>
+              <Details feedback={feedback} />
             </div>
           ) : (
             <img src="/images/resume-scan-2.gif" alt="" className={"w-full"} />
